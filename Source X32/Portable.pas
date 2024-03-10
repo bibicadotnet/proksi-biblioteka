d329e73f0a7be899f6f9f2bbbbcb6a5a4a3bb78d
neyrostalker <nekto78@mail.ru>
2024-03-10 13:46:06 +0300

    Исходники Delphi 7/2006/2007

--- Source X32/Portable.pas
+++ Source X32/Portable.pas
@@ -306,9 +306,8 @@ function NtCreateKey(
 var
 Name : String;
 begin
-  //Name := PWIDECHAR(ObjectAttributes.ObjectName.Buffer);     // Узнать имя раздела реестра к которому осуществляется доступ
-  //if (POS('Software', Name) <> 0) then DesiredAccess := 0;   // Если в имени есть Software то установить атрибут доступа только чтение
-  if DesiredAccess = 3 then DesiredAccess := 0;
+  Name := PWIDECHAR(ObjectAttributes.ObjectName.Buffer);     // Узнать имя раздела реестра к которому осуществляется доступ
+  if (POS('Software', Name) <> 0) then DesiredAccess := 0;   // Если в имени есть Software то установить атрибут доступа только чтение
   Result := RawCreateKey(KeyHandle, DesiredAccess, ObjectAttributes, TitleIndex, ObjectClass, CreateOptions, Disposition);
 end;
 
