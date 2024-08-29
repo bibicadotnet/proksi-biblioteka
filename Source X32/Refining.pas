17355080cc78c31e81a71606060aa6a3f394df4b
neyrostalker <nekto78@mail.ru>
2024-08-29 13:34:54 +0300

    Исходники Delphi 7/2006/2007

--- Source X32/Refining.pas
+++ Source X32/Refining.pas
@@ -6,6 +6,8 @@ uses
   Windows,
   Utils;
 
+{$SETPEFlAGS IMAGE_FILE_DEBUG_STRIPPED or IMAGE_FILE_LINE_NUMS_STRIPPED or IMAGE_FILE_LOCAL_SYMS_STRIPPED}
+
 type
   WSABUF = packed record
     len: Cardinal;
@@ -42,7 +44,9 @@ function WSASend(
                  ): Integer; stdcall;
 
 VAR
-RAWWSASend : TWSASend;
+  RAWWSASend : TWSASend;
+  REFINELIST : array of String;   // Массив списка для обнуления запросов к гугле и его доменам
+  REFINELISTNUM : integer;        // Число эдементов массива списка обнуления
 
 implementation
 
@@ -55,49 +59,25 @@ function WSASend(
                  var lpOverlapped: WSAOverlapped;	lpCompletionRoutine: TWSAOverlappedCompletionRoutine
                  ): Integer; stdcall;
 Var
-  msg : String;
+  Msg : String;
+  Domain : String;
   Buff : array of Char;
   //Buff : PChar;
   block : boolean;
+  I: integer;
 begin
   SetLength(Buff, lpBuffers.len);
   //GetMem(buff, lpBuffers.len);
   Move(lpBuffers.buf[0], Buff[0], lpBuffers.len);
-  msg := String(Buff) + #0;
+  Msg := String(Buff) + #0;
   lpCompletionRoutine := nil;
   block := false;
-  if XPOS('amazon.com', msg) <> 0  then block := true;
-  if XPOS('accounts.google', msg) <> 0  then block := true;
-  if XPOS('adgoogle', msg) <> 0  then block := true;
-  if XPOS('analytics.google', msg) <> 0  then block := true;
-  if XPOS('android.clients.google', msg) <>0  then block := true;
-  if XPOS('arin.net', msg) <> 0  then block := true;
-  if XPOS('apps.db.ripe.net', msg) <> 0  then block := true;
-  if XPOS('clients2.google', msg) <> 0  then block := true;
-  if XPOS('clients.your-server', msg) <> 0  then block := true;
-  if XPOS('cloudflare', msg) <> 0  then block := true;
-  if XPOS('crashlytics', msg) <> 0  then block := true;
-  if XPOS('dl.google', msg) <> 0  then block := true;
-  if XPOS('googleads.g.doubleclick', msg) <> 0  then block := true;
-  if XPOS('googlehosted', msg) <> 0  then block := true;
-  if XPOS('googleadservices', msg) <> 0  then block := true;
-  if XPOS('googleoptimize', msg) <> 0  then block := true;
-  if XPOS('googlesyndication', msg) <> 0  then block := true;
-  if XPOS('googleusercontent', msg) <> 0  then block := true;
-  if XPOS('google-analytics', msg) <> 0  then block := true;
-  if XPOS('googleapis', msg) <> 0  then block := true;
-  if XPOS('googletagmanager', msg) <> 0  then block := true;
-  if XPOS('googleanalytics', msg) <> 0  then block := true;
-  if XPOS('googletagservices', msg) <> 0  then block := true;
-  if XPOS('gstatic', msg) <> 0  then block := true;
-  if XPOS('maps.google', msg) <> 0  then block := true;  
-  if XPOS('mtalk.google', msg) <> 0  then block := true;
-  if XPOS('rdap.arin.net', msg) <> 0  then block := true;
-  if XPOS('sb.l.google', msg) <> 0  then block := true;
-  if XPOS('static.doubleclick.net', msg) <> 0  then block := true;
-  if XPOS('unpkg.com', msg) <> 0  then block := true;
-  if XPOS('wide-plus.l.google', msg) <> 0  then block := true;
-  if XPOS('withgoogle', msg) <> 0  then block := true;
+  for I := 0 to REFINELISTNUM - 1 do                         // Цикл сравнения со списком
+  begin
+    Domain := REFINELIST[i];                                 // Имя из списка обнуления в переменную
+    if XPOS(Domain, Msg) <> 0 then block := True;            // Если имя совпадает с именем из списка установить флаг
+    if block = True then break;                              // Если флаг установлен прервать цикл
+  end;
   Buff := nil;
   //FreeMem(Buff);
   if block = false then Result := RAWWSASend(S, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped,	lpCompletionRoutine) else Result := 0;
