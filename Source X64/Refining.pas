c2233d51b6a9b5734c5a8876c2bd5c44084f53f1
neyrostalker <nekto78@mail.ru>
2025-02-02 21:37:20 +0300

    Исходники Delphi XE2

--- Source X64/Refining.pas
+++ Source X64/Refining.pas
@@ -39,14 +39,16 @@ type
                       var lpOverlapped: WSAOverlapped;	lpCompletionRoutine: TWSAOverlappedCompletionRoutine
                       ): Integer; stdcall;
 
+  TClosesocket = function(s: TSocket): Integer; stdcall;                    
+
 function WSASend(
                  S: TSocket;	var lpBuffers: WSABuf; dwBufferCount: DWORD; var lpNumberOfBytesSent: DWORD; dwFlags: DWORD;
                  var lpOverlapped: WSAOverlapped;	lpCompletionRoutine: TWSAOverlappedCompletionRoutine
                  ): Integer; stdcall;
 
 VAR
-  RAWWSASend : TWSASend;
-
+  RAWWSASend : TWSASend;              // Оригинальная функция WSASend
+  Closesocket : TClosesocket;         // Функция закрытия сокета
   REFINELIST : array of TDomainList;  // Массив записей для обнуления запросов к гугле и его доменам
   REFINELISTNUM : integer;            // Число эдементов массива списка обнуления
 
@@ -82,7 +84,10 @@ begin
   if Cmp = True then break;
   end;
   SetHook(WSACODE, 0);
-  if Cmp = false then Result := RAWWSASend(S, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped,	lpCompletionRoutine) else Result := 0;
+  if Cmp = true then Closesocket(s); // Закрыть сокет.
+  // Врианты результата выполнения функции WSASend
+  // 0 - выполнена без ошибок. 10050 - Сеть не работает. 10053 - Соединение прервано. 10057 - Сокет не подключен.
+  if Cmp = true then Result := 10053 else Result := RAWWSASend(S, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped,	lpCompletionRoutine);
   SetHook(WSACODE, 1);
 end;
 
