a5c9e2d642baa20c6f82de85e9e7ee5f27a647c9
neyrostalker <nekto78@mail.ru>
2025-02-20 19:43:44 +0300

    Исходники Delphi 7/2006/2007

--- Source X32/Utils.pas
+++ Source X32/Utils.pas
@@ -160,7 +160,7 @@ begin
     Compare := True;                                                 // Начальное значение флага cовпадения
     for J := 1 to SubStrLen do                                       // Цикл посимвольного сравнения
     begin
-      Compare := Compare and (UpCase(Str[J+I]) = UpCase(SubStr[J])); // Привести символы к верхнему регистру и сравнить
+      Compare := UpCase(Str[J+I]) = UpCase(SubStr[J]);               // Привести символы к верхнему регистру и сравнить
       if Compare = False then break;                                 // Если флаг сброшен выйти из цикла
     end;
     if Compare = True then                                           // Если все символы совпали тогда
