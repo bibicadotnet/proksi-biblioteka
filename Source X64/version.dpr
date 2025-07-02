9672e6d8b6b568360b19fb816dc8092844a77407
neyrostalker <nekto78@mail.ru>
2025-07-02 09:40:11 +0300

    Обновлен файл version.dpr

--- Source X64/version.dpr
+++ Source X64/version.dpr
@@ -312,11 +312,11 @@ var
   MI   : MODULEINFO;    // Переменная типа MODULEINFO, MODULEINFO - это структура, которая содержит поле EntryPoint
   EntryADDR : PBYTE;    // Переменная указатель на адреса точки входа
 begin
+  HookPreferences;
   GetModuleInformation(GetCurrentProcess, GetModuleHandle(NIL), Addr(MI), sizeof(MODULEINFO)); // Считать информацию о процессе
   EntryADDR := MI.EntryPoint;               // Считать в переменную адрес точки входа из поля EntryPoint структуры MI
   CodeHook(EntryADDR, ADDR(REDIRECT), 1);   // Подмена адреса точки входа в процессе на адрес функции из DLL
   ADDR(ExeMain) := EntryADDR;               // Назначить адрес процедуру ExeMain равным адресу точки входа
-  HookPreferences;
 end;
 
 // Обертка для переадресации экспортируемых функций
