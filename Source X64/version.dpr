f287022ca681feca9a830d4ad2db5c8b77067ea3
neyrostalker <nekto78@mail.ru>
2025-10-05 09:49:38 +0300

    Обновлен файл version.dpr

--- Source X64/version.dpr
+++ Source X64/version.dpr
@@ -111,6 +111,16 @@ begin
   Result := Copy(DIR, 0, Len);
 end;
 
+// Функция для замены расширения файла в полном пити
+function GetIniName(DIR : string): string;
+var
+  Len: INTEGER;
+begin
+  Len := Length(DIR);
+  while (Len <> 0) and (DIR[Len] <> '.') do Dec(Len);
+  Result := Copy(DIR, 0, Len) + 'ini';
+end;
+
 // Функция для излечения значения параметра из строки
 function GetParam(IniLine : string; var IniParam : string): boolean;
 var
@@ -129,6 +139,7 @@ end;
 // Функция для чтения параметра из ini файла
 procedure READPARAM;
 var
+  IniName : String;
   IniLine : String;
   IniParam : String;
   I : integer;
@@ -153,11 +164,11 @@ begin
   FILELISTNUM := 0;
   REFINELISTNUM := 0;
 
-  GetModuleFileName(0, AppPatch, SizeOF(AppPatch));
-  APPDIR := GetAPPDir(AppPatch);
+  GetModuleFileName(HInstance, AppPatch, SizeOF(AppPatch));  // Определить пкть к dll 
+  IniName := GetIniName(AppPatch);                           // Получить путь к ini файлу 
 
   // Чтение параметров из ини файла
-  AssignFile(IniFile, APPDIR + 'Version.ini');  // Связать переменную IniFile с файлом Version.ini
+  AssignFile(IniFile, IniName);                 // Связать переменную IniFile с файлом ini
   {$I-}                                         // Выключить контроль ошибок ввода-вывода
   Reset(IniFile);                               // Открыть файл для чтения
   {$I+}                                         // Включить контроль ошибок ввода-вывода
