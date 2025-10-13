1b4013e1e357009cee1b538f1192c8f19f417037
neyrostalker <nekto78@mail.ru>
2025-10-13 21:27:53 +0300

    Обновлен файл version.dpr

--- Source X64/version.dpr
+++ Source X64/version.dpr
@@ -7,24 +7,11 @@ uses
   Hook in 'Hook.pas',
   Portable in 'Portable.pas',
   Utils in 'Utils.pas',
-  Refining in 'Refining.pas';
+  Refining in 'Refining.pas',
+  Parametrs in 'Parametrs.pas';
 
 {$R version.res}
 {$SETPEFlAGS IMAGE_FILE_DEBUG_STRIPPED or IMAGE_FILE_LINE_NUMS_STRIPPED or IMAGE_FILE_LOCAL_SYMS_STRIPPED}
-
-VAR
-  AppPatch  : array [0..MAX_PATH] of Char;
-  Proc : array [1..8] of Procedure;        // массив типа Procedure
-  FileName  : string;                      // Переменная для хранения полного имени файла
-  APPDIR    : string;                      // Переменная для хранения пути к программе
-  PARAMS    : string;                      // Переменная для хранения параметров запуска
-  ExeMain   : procedure;                   // Процедурная переменная для стартовой функции
-  IniFile   : TextFile;                    // Переменная типа TextFile для файла настроек
-
-  FULLPATCH : boolean;
-  DATADIR   : string;
-  CACHEDIR  : string;
-  RUNPARAM  : string;
 
 // Описание функций для метода dll wraper
 // Функции представляют собой джампы на адреса функций системного файла version.dll.
@@ -101,171 +88,6 @@ begin
   for i := 0 to DIRLISTNUM - 1 do DeleteDir(DELDIRLIST[i]);
 end;
 
-// Функция для определения пути к программе
-function GetAPPDir(DIR : string): string;
-var
-  Len: INTEGER;
-begin
-  Len := Length(DIR);
-  while (Len <> 0) and (DIR[Len] <> '\') do Dec(Len);
-  Result := Copy(DIR, 0, Len);
-end;
-
-// Функция для замены расширения файла в полном пити
-function GetIniName(DIR : string): string;
-var
-  Len: INTEGER;
-begin
-  Len := Length(DIR);
-  while (Len <> 0) and (DIR[Len] <> '.') do Dec(Len);
-  Result := Copy(DIR, 0, Len) + 'ini';
-end;
-
-// Функция для излечения значения параметра из строки
-function GetParam(IniLine : string; var IniParam : string): boolean;
-var
-  Len    : INTEGER;
-  SETPOS : INTEGER;
-begin
-  Result := False;
-  Len := Length(IniLine);
-  if Len = 0 then exit;
-  SETPOS := POS('=', IniLine) + 1;
-  IniParam := Copy(IniLine, SETPOS, Len - SETPOS + 1);
-  if (POS(';', IniLine) = 0) or (POS(';', IniLine) > 2) then Result := True; // Если строка не комментарий
-  if IniParam = '' then Result := False;
-end;
-
-// Функция для чтения параметра из ini файла
-procedure READPARAM;
-var
-  IniName : String;
-  IniLine : String;
-  IniParam : String;
-  I : integer;
-begin
-  REGOFF := True;                               // Значение параметра по умолчанию
-  AIDOFF := True;                               // Значение параметра по умолчанию
-  DIROFF := False;                              // Значение параметра по умолчанию
-  RMDISK := False;                              // Значение параметра по умолчанию
-  REFINE := True;                               // Значение параметра по умолчанию
-  SPFOLD := False;                              // Значение параметра по умолчанию
-  BCTOFF := True;                               // Значение параметра по умолчанию
-  STARTM := False;                              // Значение параметра по умолчанию
-  ECHOFF := False;                              // Значение параметра по умолчанию
-  FULLPATCH := True;                            // Значение параметра по умолчанию
-
-  DATADIR   := '';
-  CACHEDIR  := '';
-  RUNPARAM  := '';
-  SPECFOLDER := '';
-  COMPNAME   := '';
-
-  DIRLISTNUM := 0;
-  FILELISTNUM := 0;
-  REFINELISTNUM := 0;
-
-  GetModuleFileName(HInstance, AppPatch, SizeOF(AppPatch));  // Определить путь к dll 
-  IniName := GetIniName(AppPatch);                           // Получить путь к ini файлу 
-
-  // Чтение параметров из ини файла
-  AssignFile(IniFile, IniName);                 // Связать переменную IniFile с файлом ini
-  {$I-}                                         // Выключить контроль ошибок ввода-вывода
-  Reset(IniFile);                               // Открыть файл для чтения
-  {$I+}                                         // Включить контроль ошибок ввода-вывода
-  if IOResult = 0 then begin                    // Если ошибок нет (файл отрыт) выполнить построчное чтение файла
-  while (not EOF(IniFile)) do begin             // Пока не достигнут конец файла
-    Readln(IniFile, IniLine);                   // Прочитат строку в переменную IniLine
-    if GetParam(IniLine, IniParam) = true then  // Извлечь из строки значение параметра
-    begin
-      if POS('REGOFF=', IniLine) <> 0 then if IniParam = '1' then REGOFF := True else if IniParam = '0' then REGOFF := False;
-      if POS('AIDOFF=', IniLine) <> 0 then if IniParam = '1' then AIDOFF := True else if IniParam = '0' then AIDOFF := False;
-      if POS('DIROFF=', IniLine) <> 0 then if IniParam = '1' then DIROFF := True else if IniParam = '0' then DIROFF := False;
-      if POS('RMDISK=', IniLine) <> 0 then if IniParam = '1' then RMDISK := True else if IniParam = '0' then RMDISK := False;
-      if POS('REFINE=', IniLine) <> 0 then if IniParam = '1' then REFINE := True else if IniParam = '0' then REFINE := False;
-      if POS('SPFOLD=', IniLine) <> 0 then if IniParam = '1' then SPFOLD := True else if IniParam = '0' then SPFOLD := False;
-      if POS('BCTOFF=', IniLine) <> 0 then if IniParam = '1' then BCTOFF := True else if IniParam = '0' then BCTOFF := False;
-      if POS('STARTM=', IniLine) <> 0 then if IniParam = '1' then STARTM := True else if IniParam = '0' then STARTM := False;
-      if POS('ECHOFF=', IniLine) <> 0 then if IniParam = '1' then ECHOFF := True else if IniParam = '0' then ECHOFF := False;      
-
-      if POS('APPDIR=', IniLine) <> 0 then if IniParam = '0' then FULLPATCH := False else if IniParam = '1' then FULLPATCH := True;
-      if POS('DATADIR=', IniLine) <> 0 then if IniParam <> '' then DATADIR := IniParam;
-      if POS('CACHEDIR=', IniLine) <> 0 then if IniParam <> '' then CACHEDIR := IniParam;
-      if POS('SPECFOLDER=', IniLine) <> 0 then if IniParam <> '' then SPECFOLDER := IniParam;
-      if POS('RUNPARAM=', IniLine) <> 0 then if IniParam <> '' then RUNPARAM := IniParam;
-      if POS('COMPNAME=', IniLine) <> 0 then if IniParam <> '' then COMPNAME := IniParam;
-
-        // Заполнение массивов из списка удаления директорий
-      if POS('DeleteDir', IniLine) <> 0 then if IniParam <> '' then
-      begin
-        if DATADIR <> '' then REPLACE(IniParam, DATADIR);
-        DIRLISTNUM := DIRLISTNUM + 1;
-        SetLength(DELDIRLIST,DIRLISTNUM);
-        SetLength(BLOCKDIRLIST,DIRLISTNUM);
-        DELDIRLIST[DIRLISTNUM-1] := IniParam;
-        BLOCKDIRLIST[DIRLISTNUM-1] := DirNameDistil(IniParam);
-      end;
-
-      // Заполнение массива из списка удаления файлов
-      if POS('DeleteFile', IniLine) <> 0 then if IniParam <> '' then
-      begin
-        if DATADIR <> '' then REPLACE(IniParam, DATADIR);  // Заменить %DATADIR% на значение из параметра DATADIR
-        FILELISTNUM := FILELISTNUM + 1;
-        SetLength(FILELIST,FILELISTNUM);
-        FILELIST[FILELISTNUM-1] := IniParam;
-      end;
-
-      // Заполнить массив записей из списка обнуления запросов к гугло и его доменам
-      if POS('NullDomain', IniLine) <> 0 then if IniParam <> '' then
-      begin
-        REFINELISTNUM := REFINELISTNUM + 1;
-        SetLength(REFINELIST, REFINELISTNUM);
-        REFINELIST[REFINELISTNUM-1].len := Length(IniParam);
-        SetLength(REFINELIST[REFINELISTNUM-1].buf, REFINELIST[REFINELISTNUM-1].len + 1);  // + 1 к размеру для добавления элемента с #0
-        for I := 0 to REFINELIST[REFINELISTNUM-1].len - 1 do REFINELIST[REFINELISTNUM-1].buf[I] := AnsiChar(IniParam[I + 1]);
-      end;
-
-      end;
-    end;
-    CloseFile(IniFile);
-  end;
-end;
-
-// Функция для добавления параметров запуска
-function ADDParam(ARGS : string) : string;
-var
-  APP : String;
-  ARGSSTART : String;
-  USERDATADIR    : String;
-  DISKCACHEDIR   : String;
-
-begin
- APP := APPDIR;
-  ARGSSTART := '';
-  // Проверка наличия параметра '--single-argument'
-  if POS('--single-argument', ARGS) <> 0 then
-  begin
-    ARGSSTART := ARGS;
-    ARGS := '';
-  end;
-
-  ARGS := ARGS + '--portable' + ' ';
-  ARGS := ARGS + '--disable-features=RendererCodeIntegrity,FlashDeprecationWarning' + ' ';
-
-  if FULLPATCH = FALSE then APP := '';
-  if DATADIR <> '' then USERDATADIR := GETDIR(APP, DATADIR);
-  if CACHEDIR <> '' then DISKCACHEDIR := GETDIR(APP, CACHEDIR);
-
-  if DATADIR <> '' then ARGS := ARGS + '--user-data-dir=' + '"' + USERDATADIR + '"' + ' ';
-  if CACHEDIR <> '' then ARGS := ARGS + '--disk-cache-dir=' + '"' + DISKCACHEDIR + '"' + ' ';
-  if RUNPARAM <> '' then ARGS := ARGS + RUNPARAM + ' ';
-
-  // Если параметры не заданы
-  if POS('--user-data-dir=', ARGS) = 0 then ARGS := ARGS + '--user-data-dir=' + '"' + APPDIR + 'User Data' + '"' + ' ';
-  if POS('--disk-cache-dir=', ARGS) = 0 then ARGS := ARGS + '--disk-cache-dir=' + '"' + APPDIR + 'Cache' + '"' + ' ';
-  RESULT := ARGS + ARGSSTART;
-end;
-
 procedure STARTPORTABLE(ARGS:string);
 var
   ShellExecuteInfo: TShellExecuteInfo;     // Переменная для запуска через ShellExecuteEx
@@ -278,7 +100,6 @@ begin
   FileName := AppPatch;
   PARAMS := ADDParam(ARGS);
   APPDIR := GetAPPDir(AppPatch);
-  if DIROFF = TRUE then FDDELETE;
   //MessageBox(0, pchar(PARAMS), 'Параметры перед запуском', MB_OK); // Вывод окна перед запуском. Для отладки
   if STARTM = false then
   begin
@@ -313,6 +134,7 @@ begin
   ARG := '';
   // Перевести все параметры в одну строку
   for i := 1 to ParamCount do ARG := ARG + ParamStr(i) + ' ';
+  if DIROFF = TRUE then FDDELETE;
   // Если в командной строке нет параметров -type= и --portable тогда выполнить процедуру STARTPORTABLE
   if (POS('-type=', ARG) = 0) and (POS('--portable', ARG) = 0) then STARTPORTABLE(ARG);
   SetHook(OEPCODE, 0); // Отключить перехват точки входа
