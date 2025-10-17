7201c9404371e603f1dd5c27df1a13a76603d399
neyrostalker <nekto78@mail.ru>
2025-10-17 15:41:59 +0300

    Обновлен файл version.dpr

--- Source X64/version.dpr
+++ Source X64/version.dpr
@@ -47,15 +47,17 @@ var
   i : Integer;
   DirName : String;
   FileOp: TSHFileOpStruct;
+  Attribut: DWORD;
 begin
-  FillChar(FileOp, SizeOf(FileOp), 0);                 // Очистить структуру от случайных данных
-  FileOp.wFunc  := FO_DELETE;                          // Тип операции - удаление
-  FileOp.fFlags := FOF_SILENT or FOF_NOCONFIRMATION;   // Флаги
+  FillChar(FileOp, SizeOf(FileOp), 0);                    // Очистить структуру от случайных данных
+  FileOp.wFunc  := FO_DELETE;                             // Тип операции - удаление
+  FileOp.fFlags := FOF_SILENT or FOF_NOCONFIRMATION;      // Флаги
   for i := 0 to DIRLISTNUM - 1 do
   begin
     DirName := DELDIRLIST[i];
-    FileOp.pFrom  := PChar(DirName + #0);              // Имя и терминальный нуль для обозначения конца буфера
-    ShFileOperation(FileOp);                           // Выполнить операцию
+    Attribut := GetFileAttributes(PChar(DirName)) and 16; // Проверить наличие директории
+    FileOp.pFrom  := PChar(DirName + #0);                 // Имя и терминальный нуль для обозначения конца буфера
+    if Attribut = 16 then ShFileOperation(FileOp);        // Выполнить операцию
     end;
 end;
 
