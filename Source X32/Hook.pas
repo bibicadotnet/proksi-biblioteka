00120f266f45b76c2567ec4c72362d7c2653584b
neyrostalker <nekto78@mail.ru>
2025-10-26 12:52:22 +0300

    Обновлен файл Hook.pas

--- Source X32/Hook.pas
+++ Source X32/Hook.pas
@@ -21,6 +21,7 @@ var
   CRDCODE : HOOKDATA;                  // Для формирования перехвата CreateDirectoryW
   WSACODE : HOOKDATA;                  // Для формирования перехвата WSASend
   SSOCODE : HOOKDATA;                  // Для формирования перехвата setsockopt
+  GAICODE : HOOKDATA;                  // Для формирования перехвата getaddrinfo
 
 procedure SetHook(HOOK: HOOKDATA; OPT: byte);
 procedure CodeHook(OldProcAddress, NewProcAddress: pointer; OPT : byte = 0);
@@ -92,6 +93,14 @@ begin
     SSOCODE.FUNCADDRES := OldProcAddress;
     // Схранить начало исходной функци в структуру SSOCODE
     ReadProcessMemory(HANDLE, OldProcAddress, ADDR(SSOCODE.OLDDATA), 5, VALUE);
+  end;
+
+  if OPT = 7 then  // Это для перехвата getaddrinfo
+  begin
+    // Сохранить адрес функции в структуру
+    GAICODE.FUNCADDRES := OldProcAddress;
+    // Схранить начало исходной функци в структуру GAICODE
+    ReadProcessMemory(HANDLE, OldProcAddress, ADDR(GAICODE.OLDDATA), 5, VALUE);
   end;  
 
   // Формирование кода прыжка в прокси функцию в теле исходной функции
@@ -104,7 +113,8 @@ begin
   if OPT = 3 then  Move(CODE, KEYCODE.NEWDATA, 5);
   if OPT = 4 then  Move(CODE, CRDCODE.NEWDATA, 5);
   if OPT = 5 then  Move(CODE, WSACODE.NEWDATA, 5);
-  if OPT = 6 then  Move(CODE, SSOCODE.NEWDATA, 5);  
+  if OPT = 6 then  Move(CODE, SSOCODE.NEWDATA, 5);
+  if OPT = 7 then  Move(CODE, GAICODE.NEWDATA, 5);  
 
   // Изменить параметры доступа к области памяти
   if not VirtualProtect(OldProcAddress, 5, PAGE_EXECUTE_READWRITE, ADDR(Protect)) then exit;
