f193d5f0e2648e769e980a61a53e9e706f6312d4
neyrostalker <nekto78@mail.ru>
2025-11-23 15:12:11 +0300

    Обновлен файл version.dpr

--- Source X64/version.dpr
+++ Source X64/version.dpr
@@ -4,8 +4,6 @@ library version;
 
 uses
   Windows,
-  PSAPI,
-  ShellAPI,
   Hook in 'Hook.pas',
   Portable in 'Portable.pas',
   Utils in 'Utils.pas',
@@ -16,7 +14,6 @@ uses
 
 var
   Proc : array [1..8] of Procedure;        // массив типа Procedure
-  ExeMain   : procedure;                   // Процедурная переменная для стартовой функции
 
 // Описание функций для метода dll wraper
 // Функции представляют собой джампы на адреса функций системного файла version.dll.
@@ -42,136 +39,6 @@ exports
   GetFileVersionInfoExW name 'GetFileVersionInfoExW',
   GetFileVersionInfoSizeExW name 'GetFileVersionInfoSizeExW';
 
-// Функция удаления директорий
-procedure DDelete;
-var
-  i : Integer;
-  DirName : String;
-  FileOp: TSHFileOpStruct;
-begin
-  FillChar(FileOp, SizeOf(FileOp), 0);                    // Очистить структуру от случайных данных
-  FileOp.wFunc  := FO_DELETE;                             // Тип операции - удаление
-  FileOp.fFlags := FOF_SILENT or FOF_NOCONFIRMATION;      // Флаги
-  for i := 0 to DIRLISTNUM - 1 do
-  begin
-    DirName := DELDIRLIST[i];
-    FileOp.pFrom  := PChar(DirName + #0);                 // Имя и терминальный нуль для обозначения конца буфера
-    ShFileOperation(FileOp);                              // Выполнить операцию
-    end;
-end;
-
-// Удаление файлов по списку и шаблону
-procedure FDelete;
-var
-  i : Integer;
-  DirName : String;
-  Len: INTEGER;
-  SearchResult : TSearchRec;
-begin
-  for i := 0 to FILELISTNUM - 1 do
-  begin
-  if XPOS('*', FILELIST[i]) = 0 then DeleteFile(PChar(FILELIST[i]));
-  if XPOS('*', FILELIST[i]) <> 0 then
-    begin
-      DirName := '';
-      // Извлечь путь к файлу
-      Len := Length(FILELIST[i]);
-      while (Len <> 0) and (FILELIST[i][Len] <> '\') do Dec(Len);
-      SetString(DirName, PChar(FILELIST[i]), Len);
-      // Найти и удалить файлы по шаблону
-      if FindFirst(FILELIST[i], faAnyFile, SearchResult) = 0 then  // Найти файлы по шаблону из FILELIST[i]
-        begin
-          repeat DeleteFile(PChar(DirName + SearchResult.Name));   // Удалять найденные файлы  
-          until FindNext(SearchResult) <> 0;                       // до тех пор пока еще есть соответствующие шаблону файлы 
-          FindClose(SearchResult);                                 // Освободить ресурсы используемые процессом поиска
-        end;
-    end;
-  end;
-end;
-
-// Удаление файлов и директорий по списку
-procedure FDDelete;
-begin
-  FDelete;
-  DDelete;
-end;
-
-procedure STARTPORTABLE(ARGS:string);
-var
-  ShellExecuteInfo: TShellExecuteInfo;     // Переменная для запуска через ShellExecuteEx
-  StartupInfo: TStartupInfo;               // Переменная для запуска через CreateProcess
-  ProcessInfo: TProcessInformation;        // Переменная для запуска через CreateProcess
-begin
-  // Получить полное именя файла с использованием API функции GetModuleFileName
-  // Если вместо 0 вписать hInstance, то будет путь к имени DLL файла
-  GetModuleFileName(0, AppPatch, SizeOF(AppPatch));
-  FileName := AppPatch;
-  ExeDir := GetAPPDir(AppPatch);
-  PARAMS := ADDParam(ARGS);
-  //MessageBox(0, pchar(PARAMS), 'Параметры перед запуском', MB_OK); // Вывод окна перед запуском. Для отладки
-  if STARTM = false then
-  begin
-    FillChar(ShellExecuteInfo, SizeOf(TShellExecuteInfo), 0) ;                // Очистить структуру от случайных данных
-    ShellExecuteInfo.cbSize := sizeof(TShellExecuteInfo);                     // Размер структуры в байтах
-    ShellExecuteInfo.fMask := SEE_MASK_NOCLOSEPROCESS or SEE_MASK_FLAG_NO_UI; // Комбинация флагов, определяющих используемую часть структуры
-    ShellExecuteInfo.lpVerb := 'open';                                        // Строка, определяющее действие с файлом. 'open' запускает исполняемый файл
-    ShellExecuteInfo.lpFile := pchar(FileName);                               // Имя файла (полный путь к файлу)
-    ShellExecuteInfo.lpDirectory := pchar(ExeDir);                            // Рабочая директория программы
-    ShellExecuteInfo.nShow := SW_SHOWNORMAL;                                  // Способ отображения окна
-    ShellExecuteInfo.lpParameters := pchar(PARAMS);                           // Параметры
-    if ShellExecuteEx(ADDR(ShellExecuteInfo)) then  ExitProcess(0);           // Запустить программу
-  end
-  else begin
-    FillChar(StartupInfo, SizeOf(StartupInfo), 0);
-    StartupInfo.cb := SizeOf(StartupInfo);
-    if CreateProcess(nil, pchar(FileName + ' ' + PARAMS), nil, nil, false, 0, nil, pchar(ExeDir), StartupInfo, ProcessInfo) then
-    begin
-      ExitProcess(0);
-    end;
-  end;
-end;
-
-// Определить параметры коммандной строки, отключить шифрование и запустить программу
-procedure REDIRECT;
-var
-  ARG: String;
-  i: integer;
-begin
-  i := 2;
-  ARG := '';
-  ARG := GetCommandLine;                  // Получить полную командную строку
-
-  // Первый параметр - это выполняемая программа, он всегда передаётся в кавычках. Следующие это остальные параметры.
-  // Первый параметр нужно исключить.
-  if ARG[1] = '"' then                    // Если в начале кавычка тогда
-  begin
-    while (ARG[I] <> '"') do inc(I);      // дойти до второй кавычки
-    if (ARG[I] = '"') then inc(I);        // если кавычка то перейти за неё
-    if (ARG[I] = ' ') then inc(I);        // если пробел то перейти за него
-    Delete(ARG, 1, I-1);                  // Исключить первый параметр
-  end;
-  if ARG <> '' then ARG := ARG + ' ';     // Добавить пробел (пробел - это разделитель между параметрами)
-  
-  if DIROFF = TRUE then FDDELETE;
-  // Если в командной строке нет параметров -type= и --portable тогда выполнить процедуру STARTPORTABLE
-  if (POS('-type=', ARG) = 0) and (POS('--portable', ARG) = 0) then STARTPORTABLE(ARG);
-  SetHook(OEPCODE, 0); // Отключить перехват точки входа
-  ExeMain;             // Выполнить процедуру ExeMain
-end;
-
-// Определение и подмена адреса точки входа
-procedure RedirectEP;
-var
-  MI   : MODULEINFO;    // Переменная типа MODULEINFO, MODULEINFO - это структура, которая содержит поле EntryPoint
-  EntryADDR : PBYTE;    // Переменная указатель на адреса точки входа
-begin
-  HookPreferences;
-  GetModuleInformation(GetCurrentProcess, GetModuleHandle(NIL), Addr(MI), sizeof(MODULEINFO)); // Считать информацию о процессе
-  EntryADDR := MI.EntryPoint;               // Считать в переменную адрес точки входа из поля EntryPoint структуры MI
-  CodeHook(EntryADDR, ADDR(REDIRECT), 1);   // Подмена адреса точки входа в процессе на адрес функции из DLL
-  ADDR(ExeMain) := EntryADDR;               // Назначить адрес процедуру ExeMain равным адресу точки входа
-end;
-
 // Обертка для переадресации экспортируемых функций
 procedure RedirectEXP;
 var
@@ -196,17 +63,17 @@ end;
 // Основная стартовая функция
 procedure DllMain(fdwReason: DWORD);
 begin
-  if (fdwReason = DLL_PROCESS_ATTACH) then
+  if (fdwReason = DLL_PROCESS_ATTACH) then                // Если подключение библиотеки
   begin
-    // DisableThreadLibraryCalls(hInstance);                  // Отключить уведомления DLL_THREAD_ATTACH и DLL_THREAD_DETACH
-    READPARAM;                                                // Прочитать параметры из INI файла 
-    GetOSVer;                                                 // Определить версию ОС
-    RedirectEXP;                                              // Выполнить переадресацию функций экспорта
-    RedirectEP;                                               // Выполнить переадресацию точки входа
+    READPARAM;                                            // Прочитать параметры из INI файла
+    GetOSVer;                                             // Определить версию ОС
+    RedirectEXP;                                          // Выполнить переадресацию функций экспорта
+    if DIROFF = TRUE then FDDELETE;                       // Удалить файлы и директории если параметр включен
+    HookPreferences;                                      // Выполнить переадресацию функций
   end;
-  if (fdwReason = DLL_PROCESS_DETACH) then
+  if (fdwReason = DLL_PROCESS_DETACH) then                // Если отключение библиотеки
   begin
-    if DIROFF = TRUE then FDELETE;                            // Удалить файлы если параметр включен
+    if DIROFF = TRUE then FDELETE;                        // Удалить файлы если параметр включен
   end;
 end;
 
