0fc2a3899badd9337c922808b37a0294173b580c
neyrostalker <nekto78@mail.ru>
2025-12-14 15:08:51 +0300

    Обновлен файл Refining.pas

--- Source X64/Refining.pas
+++ Source X64/Refining.pas
@@ -272,7 +272,7 @@ begin
   begin
     Cmp := false;
     Name := '';
-    Name := PCHAR(REFINELIST[I].buf); // При таком присвоении в последнем элементе массива обязательно должен быть #0
+    SetString(Name, PCHAR(REFINELIST[I].buf), REFINELIST[I].Len);
     if XPOS(Name, Nodename) <> 0 then Cmp := true;
     if Cmp = true then break;
   end;
