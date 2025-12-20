aa7e5781b6276d05a4c5b4d5c0f235664ce35602
neyrostalker <nekto78@mail.ru>
2025-12-20 13:56:40 +0300

    Обновлен файл Refining.pas

--- Source X64/Refining.pas
+++ Source X64/Refining.pas
@@ -262,7 +262,7 @@ begin
     Cmp := false;
     Name := '';
     SetString(Name, PCHAR(REFINELIST[I].buf), REFINELIST[I].Len);
-    if XPOS(Name, Nodename) <> 0 then Cmp := true;
+    if (String(Nodename) <> '') then if XPOS(Name, Nodename) <> 0 then Cmp := true;
     if Cmp = true then break;
   end;
 
