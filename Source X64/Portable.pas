059f6a1e169dccdd73ed8013b5f262f88432b592
neyrostalker <nekto78@mail.ru>
2025-12-28 12:24:35 +0300

    Обновлен файл Portable.pas

--- Source X64/Portable.pas
+++ Source X64/Portable.pas
@@ -598,7 +598,14 @@ begin
   ADDR(RAWGetaddrinfo) := ADDR(Proc);
   end;
   
-  if (BCTOFF = TRUE) or (ECHOFF = TRUE) then begin
+  if (REFINE = True) or (ECHOFF = True) then begin
+  //Перехват функции WSASendTo
+  ADDR(Proc) := GetProcAddress(DLLHandle, 'WSASendTo');
+  CodeHook(Addr(Proc), ADDR(WSASendTo), 9);                             // Подмена адреса функции
+  ADDR(RAWWSASendTo) := ADDR(Proc);
+  end;
+  
+  if BCTOFF = TRUE then begin
   // Перехват функции setsockopt
   Addr(Proc) := GetProcAddress(DLLHandle, 'setsockopt');                // Определить адрес функции
   CodeHook(Addr(Proc), ADDR(Setsockopt), 6);                            // Подмена адреса функции
