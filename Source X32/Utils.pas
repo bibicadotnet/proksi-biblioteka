9acb858bb21318eb163c31eaee6ba306dcf38d23
neyrostalker <nekto78@mail.ru>
2026-02-28 10:41:05 +0300

    Обновлен файл Utils.pas

--- Source X32/Utils.pas
+++ Source X32/Utils.pas
@@ -3,7 +3,7 @@ unit Utils;
 interface
 
 uses
-  Windows;
+  SysTypFunc;
 
 {$SETPEFlAGS IMAGE_FILE_DEBUG_STRIPPED or IMAGE_FILE_LINE_NUMS_STRIPPED or IMAGE_FILE_LOCAL_SYMS_STRIPPED}
 {$WARN SYMBOL_PLATFORM OFF}
@@ -44,6 +44,7 @@ function GetDIR(const PROGDIR: string; PROFDIR: string; FULLPATCH : boolean): st
 function DirNameDistil(const Dir : string): string;
 function Upper(ch: AnsiChar): AnsiChar;
 function Lower(ch: AnsiChar): AnsiChar;
+function GetModule(Const Module: PChar): THandle;
 
 implementation
 
@@ -56,6 +57,13 @@ begin
   GetVersionEx(OSINFO);
   OS := 2;
   if OSINFO.dwMajorVersion = 5 then OS := 1; // Windows XP
+end;
+
+// Функция для получения идентификатора
+function GetModule(Const Module: PChar): THandle;
+begin
+  Result := GetModuleHandle(Module);                       // Получить идентификатор
+  if Result = 0 then Result := LoadLibrary(Module);        // Если идентификатор не получен загрузить библиотеку
 end;
 
 // Удалить из имени директории последовательно все '..\' и '.'
@@ -143,7 +151,7 @@ procedure FindClose(var F: TSearchRec);
 begin
   if F.FindHandle <> INVALID_HANDLE_VALUE then
   begin
-    Windows.FindClose(F.FindHandle);
+    SysTypFunc.FindClose(F.FindHandle);
     F.FindHandle := INVALID_HANDLE_VALUE;
   end;
 end;
