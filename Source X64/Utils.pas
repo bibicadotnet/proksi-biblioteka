d415668abdabf9a94fe450635ba772e2f05d27f5
neyrostalker <nekto78@mail.ru>
2026-07-21 20:22:36 +0300

    Обновлен файл Utils.pas

--- Source X64/Utils.pas
+++ Source X64/Utils.pas
@@ -107,7 +107,7 @@ procedure Replace(var PSTR: string; RSTR: string);
 var
   SETPOS : integer;
 begin
-  SETPOS := ХPOS('%DATADIR%', PSTR);
+  SETPOS := XPOS('%DATADIR%', PSTR);
   if SETPOS <> 0 then
   begin
     Delete(PSTR, SETPOS, 9);
